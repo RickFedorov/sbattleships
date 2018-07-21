446683bba31dd27a831aa97640d5ea961ac4dfc1
RickFedorov <lordshadowcz@gmail.com>
2018-07-21 19:32:47 +0200

    Process hit

--- src/engine/Ship.java
+++ src/engine/Ship.java
@@ -1,5 +1,7 @@
 package engine;
 
+import java.util.Arrays;
+
 public class Ship {
     protected Player player;
     protected Point[] points;
@@ -10,6 +12,37 @@ public class Ship {
     public Ship(Player player, ShipType shipType) {
         this.player = player;
         this.shipType = shipType;
-
     }
+
+    public void processHit(){
+        if(Arrays.stream(this.points).filter(point -> point.getStatus() == PointType.HIT).count() == this.points.length){
+            this.isDestroyed = true;
+        }
+    }
+
+    public void setPoints(Point[] points) {
+        this.points = points;
+    }
+
+    public ShipType getShipType() {
+        return shipType;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public boolean isDestroyed() {
+        return isDestroyed;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Ship{" +
+                "player=" + player.getName() +
+                ", points=" + Arrays.toString(points) +
+                "}%n");
+    }
+
+
 }
