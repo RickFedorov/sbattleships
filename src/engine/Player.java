a497e30d99ad8eb09df684e60e7727c4fe21ead0
RickFedorov <lordshadowcz@gmail.com>
2018-07-21 19:30:33 +0200

    Init Ships Check if defeated

--- src/engine/Player.java
+++ src/engine/Player.java
@@ -1,16 +1,57 @@
 package engine;
 
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import engine.log.MessageLog;
+
 public class Player {
     protected Game game;
     protected Grid grid;
-    protected Ship[] ships;
+    protected ArrayList<Ship> ships = new ArrayList<>();
     protected String name = "Unnamed player";
     protected boolean isDefeated = false;
-    protected boolean isHuman = false;
+    protected boolean isHuman;
 
     public Player(Game game, boolean isHuman) {
         this.game = game;
         this.isHuman = isHuman;
         this.grid = new Grid(this);
+
+        //number of ships can be changed later
+        this._initShips(new ShipType[]{ShipType.CARRIER, ShipType.BATTLESHIP, ShipType.CRUISER, ShipType.SUBMARINE, ShipType.DESTROYER});
+    }
+
+    private void _initShips(ShipType[] shipTypes){
+        for (ShipType shipType : shipTypes) {
+            this.ships.add(new Ship(this,shipType));
+        }
+    }
+
+    public boolean isDefeated (){
+        //check whether number of destroyed ships and if all are destroyed player is defeated.
+        if(Arrays.stream((Ship[]) this.ships.toArray()).filter(ship -> ship.isDestroyed()).count() == this.ships.size()){
+            this.isDefeated = true;
+        }
+        return this.isDefeated;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+
+
+    public ArrayList<Ship> getShips() {
+        return ships;
+    }
+
+    public Grid getGrid() {
+        return grid;
     }
 }
